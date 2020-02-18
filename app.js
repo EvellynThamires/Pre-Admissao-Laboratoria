379c922b32129d8cfc2807b30b622f8431a18b8c
EvellynThamires <61170547+EvellynThamires@users.noreply.github.com>
2020-02-18 17:26:31 -0300

    Modificações

--- app.js
+++ app.js
@@ -8,35 +8,35 @@ if(confirma == 1){
     var p1 = prompt("Quanto é 2+2? \n 1- 4 \n 2- 6 \n 3- 8" );
 
     if(p1 == 1){
-    var certo = document.getElementById("certo");
-    certo.innerHTML = ("Resposta 1")
+    var certo = document.getElementById("certo1");
+    certo.innerHTML = ("Resposta 1");
 
 }
     else{
-    var errado = document.getElementById("errado");
-    errado.innerHTML = ("Resposta incorreta")
+    var errado = document.getElementById("errado1");
+    errado.innerHTML = ("Resposta1")
 }
 
     var p2 = prompt("Quanto é 6*6? \n 1- 28 \n 2 -36 \n 3 -40" );
 
     if(p2 == 2){
-    var certo = document.getElementById("certo");
+    var certo = document.getElementById("certo2");
     certo.innerHTML = ("Resposta 2")
 }
     else{
-    var errado = document.getElementById("errado");
-    errado.innerHTML = ("Resposta incorreta")
+    var errado = document.getElementById("errado2");
+    errado.innerHTML = ("Resposta 2")
 }
 
     var p3 = prompt("Quanto é 5-3? \n 1- 2 \n 2- 6 \n 3- 3");
 
     if(p3 == 1){
-    var certo = document.getElementById("certo");
+    var certo = document.getElementById("certo3");
     certo.innerHTML = ("Resposta 3")
 }
     else{
-    var errado = document.getElementById("errado");
-    errado.innerHTML = ("Resposta incorreta")
+    var errado = document.getElementById("errado3");
+    errado.innerHTML = ("Resposta 3")
 }
 }
 else{
